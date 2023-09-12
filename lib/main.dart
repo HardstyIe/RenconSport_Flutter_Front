63cacd4a67998a0a5052bd090aca73ef5500f2f6
HardstyIe <Hardstyle.dd@gmail.com>
2023-09-12 20:11:26 +0200

    wip(auth):working on the social auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,159 @@
-import 'package:flutter/material.dart';
-import 'package:renconsport/services/theme.dart';
-import 'package:renconsport/widgets/homepage/homepage.dart';
-import 'package:renconsport/widgets/appbar.dart';
+/// -----------------------------------
+///          External Packages
+/// -----------------------------------
 
-void main() {
-  runApp(const MyApp());
+import 'dart:async';
+
+import 'package:auth0_flutter/auth0_flutter.dart';
+import 'package:flutter/material.dart';
+
+const appScheme = 'HTTPS';
+
+/// -----------------------------------
+///           Profile Widget
+/// -----------------------------------
+class Profile extends StatelessWidget {
+  final Future<void> Function() logoutAction;
+  final UserProfile user;
+
+  const Profile(this.logoutAction, {required this.user, final Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          width: 150,
+          height: 150,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.blue, width: 4),
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: NetworkImage(user.pictureUrl?.toString() ?? ''),
+            ),
+          ),
+        ),
+        const SizedBox(height: 24),
+        Text('Name: ${user.name}'),
+        const SizedBox(height: 48),
+        ElevatedButton(
+          onPressed: () async {
+            await logoutAction();
+          },
+          child: const Text('Logout'),
+        ),
+      ],
+    );
+  }
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+/// -----------------------------------
+///            Login Widget
+/// -----------------------------------
+class Login extends StatelessWidget {
+  final Future<void> Function() loginAction;
+  final String loginError;
+
+  const Login(this.loginAction, this.loginError, {final Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ElevatedButton(
+          onPressed: () async {
+            await loginAction();
+          },
+          child: const Text('Login'),
+        ),
+        Text(loginError ?? ''),
+      ],
+    );
+  }
+}
+
+/// -----------------------------------
+///                 App
+/// -----------------------------------
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+/// -----------------------------------
+///              App State
+/// -----------------------------------
+
+class _MyAppState extends State<MyApp> {
+  Credentials? _credentials;
+  late Auth0 auth0;
+  bool isBusy = false;
+  late String errorMessage;
+
+  void initState() {
+    super.initState();
+
+    auth0 = Auth0('dev-rsb7ct14ymtfm6w1.eu.auth0.com',
+        'eQzcSr1qbGm3l3ScTiJZc9bk7iulxBq2');
+    errorMessage = '';
+  }
+
+  Future<void> loginAction() async {
+    setState(() {
+      isBusy = true;
+      errorMessage = '';
+    });
+
+    try {
+      final Credentials credentials =
+          await auth0.webAuthentication(scheme: appScheme).login();
+
+      setState(() {
+        isBusy = false;
+        _credentials = credentials;
+      });
+    } on Exception catch (e, s) {
+      debugPrint('login error: $e - stack: $s');
+
+      setState(() {
+        isBusy = false;
+        errorMessage = e.toString();
+      });
+    }
+  }
+
+  Future<void> logoutAction() async {
+    await auth0.webAuthentication(scheme: appScheme).logout();
+
+    setState(() {
+      _credentials = null;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme:  CustomTheme.defaultTheme,
-        home: Scaffold(
-          body: HomePage(),
-          appBar: CustomAppBar(),
-
+      title: 'Auth0 Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Auth0 Demo'),
         ),
-
-
+        body: Center(
+          child: isBusy
+              ? const CircularProgressIndicator()
+              : _credentials != null
+                  ? Profile(logoutAction, user: _credentials!.user!)
+                  : Login(loginAction, errorMessage),
+        ),
+      ),
     );
   }
 }
